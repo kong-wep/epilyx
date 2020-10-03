bdaf503fe4246327c3e21bc4c4bfd4d7c4dd9ac8
toftpokk <toftpokk@protonmail.com>
2020-10-03 18:07:05 +0700

    Made electric drill chargable

--- epilyx_industry/drill.lua
+++ epilyx_industry/drill.lua
@@ -4,18 +4,18 @@
 local function drill_no_power(itemstack, user, node, digparams)
    item_name = itemstack:get_name()
    if itemstack:get_wear()+digparams.wear <= 65535 then
-      itemstack:add_wear(digparams.wear)
-      return itemstack
+     itemstack:add_wear(digparams.wear)
+     return itemstack
    else
 
-      itemstack:add_wear(65535-itemstack:get_wear())
-      local tool_cap = itemstack:get_tool_capabilities()["groupcaps"]
-      for k,grp in pairs(itemstack:get_tool_capabilities()["groupcaps"]) do
-         tool_cap["times"] = {}
-      end
-      epilyx.chat("out of power")
-      itemstack:get_meta():set_tool_capabilities(tool_cap)
-      return itemstack
+     itemstack:add_wear(65535-itemstack:get_wear())
+     local tool_cap = itemstack:get_tool_capabilities()["groupcaps"]
+     for k,grp in pairs(itemstack:get_tool_capabilities()["groupcaps"]) do
+       tool_cap["times"] = {}
+     end
+     epilyx.chat("out of power")
+     itemstack:get_meta():set_tool_capabilities(tool_cap)
+     return itemstack
    end
 end
 
@@ -29,25 +29,25 @@ local WEAR_TABLE = {
 -- Register Tools
 local function default_drill(material_name,description)
  minetest.register_tool("epilyx_industry:"..material_name.."_drill", {
-    description = description,
-    inventory_image = material_name .."_drill.png",
-    tool_capabilities = {
-        full_punch_interval = 1.5,
-        max_drop_level = 0,
-        groupcaps = {
-            crumbly = {
-                maxlevel = 1,
-                uses = 10,
-                times = { [1]=0.00, [2]=0.50, [3]=0.00 }
-            },
-            cracky = {
-                maxlevel = 1,
-                uses = 1,
-                times = { [1]=0.00, [2]=0.50, [3]=0.00 }
-            },
-        },
-        damage_groups = {fleshy=2},
-    },
+   description = description,
+   inventory_image = material_name .."_drill.png",
+   tool_capabilities = {
+      full_punch_interval = 1.5,
+      max_drop_level = 0,
+      groupcaps = {
+         crumbly = {
+            maxlevel = 1,
+            uses = 10,
+            times = { [1]=0.00, [2]=0.50, [3]=0.00 }
+         },
+         cracky = {
+            maxlevel = 1,
+            uses = 1,
+            times = { [1]=0.00, [2]=0.50, [3]=0.00 }
+         }
+      },
+      damage_groups = {fleshy=2},
+   },
 })
 minetest.register_alias(material_name.."_drill","epilyx_industry:"..material_name.."_drill")
 end
@@ -55,35 +55,39 @@ end
 default_drill("copper","Copper Drill")
 
 minetest.register_tool("epilyx_industry:electric_drill", {
-    description = "Electric Drill",
-    inventory_image = "electric_drill.png",
-    tool_capabilities = {
-        full_punch_interval = 1.5,
-        max_drop_level = 0,
-        groupcaps = {
-            crumbly = {
-                maxlevel = 1,
-                uses = 10,
-                times = { [1]=0.00, [2]=0.50, [3]=0.00 }
-            },
-            cracky = {
-                maxlevel = 1,
-                uses = 1,
-                times = { [1]=0.00, [2]=0.50, [3]=0.00 }
-            },
-        },
-        damage_groups = {fleshy=2},
-    },
-    after_use = drill_no_power
+   description = "Electric Drill",
+   inventory_image = "electric_drill.png",
+   tool_capabilities = {
+      full_punch_interval = 1.5,
+      max_drop_level = 0,
+      groupcaps = {
+         crumbly = {
+            maxlevel = 1,
+            uses = 10,
+            times = { [1]=0.00, [2]=0.50, [3]=0.00 }
+         },
+         cracky = {
+            maxlevel = 1,
+            uses = 1,
+            times = { [1]=0.00, [2]=0.50, [3]=0.00 }
+         },
+      },
+      damage_groups = {fleshy=2},
+   },
+   after_use = drill_no_power,
+   groups = {
+     epilyx_chargable = 1
+   }
 })
 minetest.register_alias("electric_drill","epilyx_industry:electric_drill")
+
 -- End Register Tools
 
 -- Register Items
 local function default_head(material_name,description)
 minetest.register_craftitem("epilyx_industry:"..material_name.."_drill_head", {
-    description = description,
-    inventory_image = material_name.."_drill_head.png",
+   description = description,
+   inventory_image = material_name.."_drill_head.png",
 })
 minetest.register_alias(material_name.."_drill_head","epilyx_industry:"..material_name.."_drill_head")
 end
@@ -92,7 +96,7 @@ default_head("copper","Copper Drill Head")
 
 default_head("tin","Tin Drill Head")
 
-default_head("obsidian","Obsidian Drill Head")
+default_head("steel","Steel Drill Head")
 
 default_head("obsidian","Obsidian Drill Head")
 
@@ -101,8 +105,8 @@ default_head("diamond","Diamond Drill Head")
 default_head("electric","Electric Drill Head")
   
 minetest.register_craftitem("epilyx_industry:drill_base", {
-    description = "Drill Base",
-    inventory_image = "drill_base.png",
+   description = "Drill Base",
+   inventory_image = "drill_base.png",
 })
 minetest.register_alias("drill_base","epilyx_industry:drill_base")
 -- End Register Items
--- epilyx_industry/power.lua
+++ epilyx_industry/power.lua
@@ -1,68 +1,83 @@
 --[[
    Power Items
 ]]
+
 -- Form Specs
 local function charger_menu()
-   local text = "Charger"
+   local text = "hello world"
    local formspec = {
-        "formspec_version[3]",
-        "size[10,7]",
-        "label[0.375,0.5;", minetest.formspec_escape(text), "]",
-        "list[context;input;0,0;1,1;]",
-        "list[current_player;main;0.15,3;8,4;]"
-    }
+      "formspec_version[3]",
+      "size[10.25,8.5]",
+      "list[context;input;0,0;2,2]",
+      "list[current_player;main;0.25,3.5;8,4]",
+      "listring[]"
+   }
    return table.concat(formspec, "")
 end
+
 local function boiler_menu()
    local text = "hello world"
    local formspec = {
-        "formspec_version[3]",
-        "size[6,3.476]",
-        "label[0.375,0.5;", minetest.formspec_escape(text), "]",
-        "field[0.375,1.25;5.25,0.8;number;Number;]",
-        "button[1.5,2.3;3,0.8;guess;Guess]"
-    }
+      "formspec_version[3]",
+      "size[6,3.476]",
+      "label[0.375,0.5;", minetest.formspec_escape(text), "]",
+      "field[0.375,1.25;5.25,0.8;number;Number;]",
+      "button[1.5,2.3;3,0.8;guess;Guess]"
+   }
    return table.concat(formspec, "")
 end
 --
 
 -- Register Blocks
 minetest.register_node("epilyx_industry:charger",{
-    description = "Charger",
-    tiles = {"charger.png"},
-    is_ground_content = false,
-    groups = {metallic=3,cracky=1},
-    after_place_node = function(pos, placer)
-        local meta = minetest.get_meta(pos)
-    end,
-    on_receive_fields = function(pos, formname, fields, player)
-        if fields.quit then
-           epilyx.chat("a")
-            return
-        end
-
-        print(fields.x)
-    end
+   description = "Charger",
+   tiles = {"charger.png"},
+   is_ground_content = false,
+   groups = {metallic=3,cracky=1},
+   after_place_node = function(pos, placer)
+      local meta = minetest.get_meta(pos)
+      meta:set_string("formspec",charger_menu())
+      local inv = meta:get_inventory()
+      inv:set_size('input',1)
+      minetest.get_node_timer(pos):start(1)
+   end,
+   on_receive_fields = function(pos, formname, fields, player)
+      local name = minetest.get_meta(pos):get_inventory():get_stack("input",1):get_name()
+      local c = minetest.get_item_group(name, "epilyx_chargable")
+      if c == 1 then
+      end
+   end,
+   on_timer = function(pos,elapsed)
+      -- Charge
+      local stack = minetest.get_inventory({type="node",pos=pos}):get_stack("input",1)
+      local name = stack:get_name()
+      if minetest.get_item_group(name, "epilyx_chargable") then
+         local w = -3276
+         stack:add_wear(w)
+         minetest.get_meta(pos):get_inventory():set_stack("input",1,stack)
+      end
+      return true
+   end
 })
 minetest.register_alias("charger","epilyx_industry:charger")
 
 minetest.register_node("epilyx_industry:boiler",{
-    description = "Boiler",
-    tiles = {"charger.png"},
-    is_ground_content = false,
-    groups = {metallic=3,cracky=1},
-    after_place_node = function(pos, placer)
-        local meta = minetest.get_meta(pos)
-        meta:set_string("formspec",boiler_menu())
-    end,
-    on_receive_fields = function(pos, formname, fields, player)
-        if fields.quit then
-           epilyx.chat("a")
-            return
-        end
+   description = "Boiler",
+   tiles = {"charger.png"},
+   is_ground_content = false,
+   groups = {metallic=3,cracky=1},
+   after_place_node = function(pos, placer)
+      local meta = minetest.get_meta(pos)
+      meta:set_string("formspec",boiler_menu())
+   end,
+   on_receive_fields = function(pos, formname, fields, player)
+      if fields.quit then
+         epilyx.chat("a")
+         return
+      end
 
-        print(fields.x)
-    end
+      print(fields.x)
+   end
 })
 minetest.register_alias("boiler","epilyx_industry:boiler")
 -- End Register Blocks
